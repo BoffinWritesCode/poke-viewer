fc5f23fb27ecedeb91b61805c64536a0f015072b
BoffinWritesCode <reachjoshjohnson@gmail.com>
2022-03-04 14:59:48 +0000

    renamed files and components, added quick background

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import './app.css';
+import NavBar from './navbar.js';
+
+/*
+Notes:
+So the app itself is going to be:
+
+- Navigation bar at the top, just for anything that could be useful.
+  This would contain a drop-down menu that allows you to see all the different pokemon
+  in order, and pick which one you'd like to look at.
+- The main UI in the center. I'd like to design it so the UI looks like a pokemon card,
+  and switching to a new pokemon moves the card to the side, bringing the new card in.
+- Arrows on the left and right, that allow you to view the pokemon one above or below
+  in the pokedex.
+*/
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+        <NavBar />
     </div>
   );
 }
---
+++ src/navbar.js
@@ -0,0 +1,10 @@
+import './navbar.css';
+
+function NavBar() {
+  return (
+    <div className="navigation-bar">
+    </div>
+  );
+}
+
+export default NavBar;
