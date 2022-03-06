d0a091d7ec3da7d77893e347fa21c1ad74a3f895
BoffinWritesCode <reachjoshjohnson@gmail.com>
2022-03-06 19:59:13 +0000

    added selection for the pokemon in the large list

--- src/components/pokemonList.js
+++ src/components/pokemonList.js
@@ -6,6 +6,7 @@ import PokemonListItem from './pokemonListItem';
 
 class PokemonList extends React.Component {
     #callback;
+    #selectedChild;
     values = [];
     constructor(props) {
         super(props);
@@ -13,6 +14,18 @@ class PokemonList extends React.Component {
         this.state = {
             values: []
         };
+        this.clearSelected = this.clearSelected.bind(this);
+        this.setSelected = this.setSelected.bind(this);
+    }
+    clearSelected() {
+        this.#selectedChild = undefined;
+    }
+    setSelected(child) {
+        if (this.#selectedChild) {
+            this.#selectedChild.setSelected(false);
+        }
+        this.#selectedChild = child;
+        this.#selectedChild.setSelected(true);
     }
     componentDidMount() {
         // dont add callback if it's already been added
@@ -20,7 +33,7 @@ class PokemonList extends React.Component {
 
         this.#callback = (pokemon) => {
             this.values.push(
-                <PokemonListItem key={pokemon.ref_name} id={pokemon.data.id} icon={pokemon.data.icon} label={pokemon.data.name} stylingClass="pokemon-list-item" />
+                <PokemonListItem key={pokemon.ref_name} parent={this} id={pokemon.data.id} icon={pokemon.data.icon} label={pokemon.data.name} stylingClass="pokemon-list-item" />
             );
             this.values.sort((a, b) => a.props.id - b.props.id);
             this.setState({
--- src/components/pokemonListItem.js
+++ src/components/pokemonListItem.js
@@ -4,14 +4,28 @@ import React from 'react';
 class PokemonListItem extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            selected: false
+        };
         this.icon = props.icon;
         this.label = props.label;
         this.id = props.id;
+        this.listParent = props.parent;
+        this.onClicked = this.onClicked.bind(this);
+        this.setSelected = this.setSelected.bind(this);
+    }
+    onClicked() {
+        this.listParent.setSelected(this);
+    }
+    setSelected(value) {
+        this.setState({
+            selected: value
+        });
     }
     render() {
         const img = this.icon ? <img src={this.icon} /> : "";
         return (
-            <div className={'pokemon-list-item'}>
+            <div onClick={this.onClicked} className={`pokemon-list-item ${this.state.selected ? 'pokemon-list-item-selected' : ''}`}>
                 {img}
                 <label className={this.icon ? "" : "pokemon-list-item-no-img"}>{this.label}</label>
             </div>
